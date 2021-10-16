8b233cd4d91a0487f34b0c1c83b074ebeb2f6914
Stanislav Konchanin <konchanin.stas@gmail.com>
2021-10-16 12:46:00 +0300

    h

---
+++ Colorado/GeometryDataStructures/GeometryStructures/Geometry3D/Sphere.cs
@@ -0,0 +1,31 @@
+﻿using Colorado.GeometryDataStructures.GeometryStructures.BaseGeometryStructures;
+using Colorado.GeometryDataStructures.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colorado.GeometryDataStructures.GeometryStructures.Geometry3D
+{
+    public class Sphere : GeometryObject
+    {
+        public Sphere(Point centerPoint, double radius)
+        {
+            Center = centerPoint;
+            Radius = radius;
+        }
+
+        public Point Center { get; }
+
+        public double Radius { get; }
+
+        public override BoundingBox BoundingBox { get; }
+
+        private BoundingBox GetBoundingBox()
+        {
+            return new BoundingBox(Center + Vector.XAxis * Radius + Vector.YAxis * Radius + Vector.ZAxis * Radius,
+                Center - Vector.XAxis * Radius - Vector.YAxis * Radius - Vector.ZAxis * Radius);
+        }
+    }
+}
